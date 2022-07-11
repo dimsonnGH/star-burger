94c393a241f1f754783de87bd6b8350e3f2d591b
dimsonnGH <dimsonn@mail.ru>
2022-07-11 18:30:39 +0400

    Moving geocoder functions to coordinates app.

---
+++ coordinates/geocoder_functions.py
@@ -0,0 +1,38 @@
+from .models import Location
+import requests
+from django.conf import settings
+
+def fetch_coordinates(apikey, address):
+    try:
+        location = Location.objects.only("longitude", "latitude").get(address=address)
+        return location.longitude, location.latitude
+    except Location.DoesNotExist:
+        pass
+
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        lon = None
+        lat = None
+    else:
+        most_relevant = found_places[0]
+        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+
+    Location.objects.create(address=address, longitude=lon, latitude=lat)
+    return lon, lat
+
+
+def get_address_coordinates(place_coordinates, address):
+    coordinates = place_coordinates.get(address)
+    place_coordinates_modified = {**place_coordinates}
+    if not coordinates:
+        coordinates = fetch_coordinates(settings.YANDEX_GEOCODER_KEY, address)
+        place_coordinates_modified[address] = coordinates
+    return coordinates, place_coordinates_modified
--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -3,46 +3,8 @@ from django.db.models import Sum, F
 from django.core.validators import MinValueValidator
 from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
-from django.conf import settings
 from geopy import distance
-from coordinates.models import Location
-import requests
-
-
-def fetch_coordinates(apikey, address):
-    try:
-        location = Location.objects.only("longitude", "latitude").get(address=address)
-        return location.longitude, location.latitude
-    except Location.DoesNotExist:
-        pass
-
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        lon = None
-        lat = None
-    else:
-        most_relevant = found_places[0]
-        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-
-    Location.objects.create(address=address, longitude=lon, latitude=lat)
-    return lon, lat
-
-
-def get_address_coordinates(place_coordinates, address):
-    coordinates = place_coordinates.get(address)
-    place_coordinates_modified = {**place_coordinates}
-    if not coordinates:
-        coordinates = fetch_coordinates(settings.YANDEX_GEOCODER_KEY, address)
-        place_coordinates_modified[address] = coordinates
-    return coordinates, place_coordinates_modified
+from coordinates.geocoder_functions import get_address_coordinates
 
 
 class Restaurant(models.Model):
