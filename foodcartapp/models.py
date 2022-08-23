623d757cfb989ab851507e8a7830dbdc0e20d334
dimsonnGH <dimsonn@mail.ru>
2022-08-23 22:12:59 +0400

    Fixing a misprint.

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -141,12 +141,12 @@ class OrderQuerySet(models.QuerySet):
         addresses = addresses + [restaurant.address for restaurant in Restaurant.objects.all()]
 
         locations = Location.objects.filter(address__in=set(addresses))
-        coordinates_cashe = {location.address: (location.longitude, location.latitude) for location in locations}
+        coordinates_cache = {location.address: (location.longitude, location.latitude) for location in locations}
 
         for order in orders:
             order_restaurants = set()
             restaurants_with_distance = []
-            order_address_coordinates, coordinates_cashe = get_address_coordinates(coordinates_cashe, order.address)
+            order_address_coordinates, coordinates_cache = get_address_coordinates(coordinates_cache, order.address)
 
             order_items = order.items.all()
             for order_item in order_items:
@@ -158,7 +158,7 @@ class OrderQuerySet(models.QuerySet):
                     order_restaurants = set(item_restaurants)
 
             for restaurant in order_restaurants:
-                restorant_coordinates, coordinates_cashe = get_address_coordinates(coordinates_cashe,
+                restorant_coordinates, coordinates_cache = get_address_coordinates(coordinates_cache,
                                                                                    restaurant.address)
                 if order_address_coordinates and restorant_coordinates:
                     distance_to = round(distance.distance(restorant_coordinates, order_address_coordinates).km, 2)
